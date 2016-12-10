703c90eaedc416a812826b5a61f57e8b1de5f149
Manuel Medina <jmedina_avila@hotmail.com>
2016-12-09 20:22:39 -0600

    programa terminado...

---
+++ Tarea Nombre/src/mx/com/everis/tallerjava/tarea/ModificaNombre.java
@@ -0,0 +1,12 @@
+package mx.com.everis.tallerjava.tarea;
+
+public class ModificaNombre {
+
+	public static void main(String[] args) {
+		
+		String name ="Juan Manuel Medina Avila";
+		
+		System.out.println(name.replace(" ", "@"));
+	}
+	
+}
